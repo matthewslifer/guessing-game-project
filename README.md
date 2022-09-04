9dd69ad640d15c705ca76584f82f6dca256b4715
Matthew Slifer <mslifer6@gmail.com>
2022-09-03 19:00:16 -0700

    Final commit

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ Interaction game using JavaScript where the user has to guess a secret number
 chosen at random. After each guess, the program will respond and give a hint
 to the user.
 
-The user will guess a number that is between 0 and 100. The program runs through
-node.js.
+The user will enter in a minimum value and a maximum value. The program will then
+select a random whole integer in between those two values. The user than guesses
+the correct number while the program will give hints when the guess is incorrect.
+
+This program can be run on node.js
--- guessing-game.js
+++ guessing-game.js
@@ -6,7 +6,31 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-secretNumber = 23
+//Asks user for a minimum and maximum value, passes those values into the randomInRange function, then calls on askGuess function to begin gameplay.
+let askRange = () => {
+    rl.question("Enter a minimim value: ", minimum => {
+        let min = Number(minimum);
+
+        rl.question("Enter a maximum value: ", maximum => {
+            let max = Number(maximum);
+            console.log("I am thinking of a number between " + min + " and " + max + "...");
+            randomInRange(min, max);
+            askGuess();
+        })
+    })
+}
+
+//Variable for number randomly selected
+var secretNumber = ' ';
+
+//Accepts a minimum and maximum value and then selects a random whole integer between those two values
+let randomInRange = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    secretNumber = Number(Math.floor(Math.random() * (max - min) + min));
+    return secretNumber;
+}
+
 
 //Checks if number is too big, too small, or is correct
 let checkGuess = function(num) {
@@ -38,4 +62,5 @@ let askGuess = () => {
     });
 }
 
-askGuess();
+//Begins game
+askRange()
